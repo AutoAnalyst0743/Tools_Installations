e7c02427f9a512c8025c56e8ec079b65d18525d2
AutoAnalyst0743 <testautomationefforts1@gmail.com>
2025-08-22 13:06:32 +0530

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ export function Navbar() {
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center space-x-2">
             <GraduationCap className="h-8 w-8 text-blue-600" />
-            <span className="text-xl font-bold text-gray-900">Testing Professor</span>
+            <span className="text-xl font-bold text-gray-900">Isha Training Solutions</span>
           </div>
           
           {/* Desktop Menu */}
